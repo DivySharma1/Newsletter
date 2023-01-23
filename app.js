7c154710a9c9a132cdec386333d25580780bef18
DivySharma1 <94278566+DivySharma1@users.noreply.github.com>
2023-01-24 00:29:03 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,74 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const request=require("request");
+const https=require("https");
+const { log } = require("console");
+
+const app=express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req,res){
+    res.sendFile(__dirname+ "/signup.html");
+});
+
+app.post("/", function(req,res){
+    var firstName=req.body.firstname;
+    var lastName=req.body.lastname;
+    var email=req.body.email;
+
+    var data={
+        members:[
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields:{
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    var jsonData=JSON.stringify(data);
+
+    const url="https://us11.api.mailchimp.com/3.0/lists/43fad840ab";
+    const options={
+        method:"post",
+        auth: "divy:5f6b56bd78dfd90e6c568a525a21651e-us11"
+    }
+
+    const request=https.request(url, options, function(response){
+        var code=response.statusCode;
+        var errorCode=response.error_count;
+        console.log(code);
+        if (code==200 && errorCode==0) {
+            res.sendFile(__dirname+ "/success.html");
+        } 
+        else {
+            res.sendFile(__dirname+ "/failure.html");
+        }
+
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        });
+    });
+
+    request.write(jsonData);
+    request.end();
+
+});
+
+app.post("/failure", function(req, res){
+    res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("server is running on port 3000");
+});
+
+// api key
+// 5f6b56bd78dfd90e6c568a525a21651e-us11
+
+// list id
+// 43fad840ab
